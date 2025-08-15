b1d618dbd1691a1e415015e87ace720b5783ec08
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2025-08-15 18:06:55 +0530

    Main Model

---
+++ BlockchainSimulatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  BlockchainSimulatorApp.swift
+//  BlockchainSimulator
+//
+//  Created by Suryanandan Babbar on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
